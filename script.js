2b25c23a4aa38a9f8c9da5d2dd8212cd07449985
dhulam07 <chandudhulam794@gmail.com>
2025-11-25 16:23:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-// complete this js code
-function Person(name, age) {}
+class Person{
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+	greet(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+	}
+}
+class Employee{
+	constructor(name,age,jobTitle){
+		this.name=name;
+		this.age=age;
+		this.jobTitle=jobTitle;
+	}
+	jobGreet(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+const person = new Person("Alice", 25);
+person.greet();
+
+const employee = new Employee("Bob", 30, "Manager");
+employee.jobGreet();
+
+
+
+
+
+
+
+// complete this js code
+// function Person(name, age) {
+	
+// }
+
+// function Employee(name, age, jobTitle) {}
 
 // Do not change code below this line
 window.Person = Person;
